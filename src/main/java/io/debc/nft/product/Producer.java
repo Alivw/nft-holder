6b2c0e82035ffbe6f3ddb32ff36bd16a3d03032e
jalivv <shizuwei99@gmail.com>
2022-12-29 11:36:11 +0800

    remove useless

--- src/main/java/io/debc/nft/product/Producer.java
+++ src/main/java/io/debc/nft/product/Producer.java
@@ -17,8 +17,6 @@ import java.util.stream.Collectors;
  **/
 public class Producer {
 
-    public static final String TRANSFER_ID = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
-    public static final String TRANSFER_SINGLE_ID = "0xc3d58168c5ae7397731d063d5bbf3d657854427343f4c083240f7aacaa2d0f62";
     private Produce produce;
 
     public Producer(Produce produce) {
@@ -27,27 +25,11 @@ public class Producer {
 
     private static Predicate<Log> distinct() {
         return log -> {
-            Map<String, Log> map = new ConcurrentHashMap<>();
             List<String> topics = log.getTopics();
-
-
             if (log.getTopics() == null) {
                 return false;
             }
-
-            if (topics.size() != 4) {
-                return false;
-            }
-
-            //if (topics.get(0).equals(TRANSFER_ID)) {
-            //    return map.put(log.getAddress() + topics.get(3), log) == null;
-            //}
-
-            if (TRANSFER_SINGLE_ID.equals(topics.get(0))) {
-                return map.put(log.getAddress() + topics.get(2) + topics.get(3) + log.getData().substring(2, 66), log) == null;
-            }
-
-            return true;
+            return topics.size() == 4;
         };
     }
 
