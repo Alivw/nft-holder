51dd5637d498f49c71935f0d66b6239fbcb1bbeb
jalivv <shizuwei99@gmail.com>
2023-01-03 13:23:38 +0800

    remove idea

--- src/main/java/io/debc/nft/TestMain.java
+++ src/main/java/io/debc/nft/TestMain.java
@@ -1,51 +1,10 @@
 package io.debc.nft;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
-import org.elasticsearch.script.mustache.SearchTemplateAction;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.Function;
-
 /**
  * @description:
  * @author: Jalivv
  * @create: 2022-12-12 13:25
  **/
 public class TestMain {
-    static CacheLoader cacheLoader = new CacheLoader();
-
-    public static void main(String[] args) throws Exception {
-        ExecutorService pool = Executors.newFixedThreadPool(10);
-
-        Cache<String, Integer> cache = Caffeine.newBuilder().build();
-        for (int i = 0; i < 20; i++) {
-            final String j =""+ i % 5;
-            pool.execute(()->{
-                String intern = j.intern();
-                synchronized (j) {
-                    Integer c = cache.getIfPresent(j);
-                    if (c == null) {
-                        System.out.println("not hit cache");
-                        cache.put("" + j, 4);
-                    }else {
-                        System.out.println(c);
-                    }
-                }
-
-
-            });
-        }
-    }
-
-
-    static class CacheLoader implements Function<String, Integer> {
-        @Override
-        public Integer apply(String s) {
-            System.out.println("not hit cache");
-            return Integer.parseInt(s) * 2;
-        }
-    }
 
 }
